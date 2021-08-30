baeacda8a72e0cf7c0a023c81c82fd606d94bf36
Agamuyi Precious Edobor <preciousagamuyi@gmail.com>
2021-08-30 14:10:32 -0700

    removed a comment line

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(express.json());
 
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
     let method = req.body._method
     delete req.body._method
     return method
