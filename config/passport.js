90eecce3362981e43831769ec3849949fac2fcfc
Agamuyi Precious Edobor <preciousagamuyi@gmail.com>
2021-08-31 01:11:16 -0700

    updated a line

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ const User = require('../models/User')
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: 'https://tell-your-stories.herokuapp.com/auth/google/callback',
     },
       async (accessToken, refreshToken, profile, done ) => {
         const newUser ={
